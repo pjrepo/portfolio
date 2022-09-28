1d7a614e0c95fb47279dc79f43a48aa8cc1d4d68
pjrepo <pavanjegurupati75@gmail.com>
2022-09-28 20:22:03 +0530

    chore: portfolio website source code addition

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -9,20 +9,24 @@ import taskmanager from "../../assets/taskmanager.webp";
 import cocktailsapi from "../../assets/cocktailsapi.webp";
 import sortingvisualizer from "../../assets/sortingvisualizer.webp";
 import expensetracker from "../../assets/expensetracker.webp";
+import portfoliowebsite from "../../assets/portfoliowebsite.webp";
 
 const projects = [
   {
     i: 1,
     image: colorpicker,
     title: "COLOR PICKER EXTENSION",
-    description: "#Manifest_V3 #Javascript",
+    description: [
+      { j: "1", technology: "Manifest_V3" },
+      { j: "2", technology: "Javascript" },
+    ],
     sourceCode: "https://github.com/pjrepo/color-picker-extension",
   },
   {
     i: 2,
     image: tictactoe,
     title: "TIC TAC TOE",
-    description: "#React",
+    description: [{ j: "1", technology: "React Js" }],
     liveLink: "https://tic-tac-toe-pjrepo.vercel.app/",
     sourceCode: "https://github.com/pjrepo/tic-tac-toe",
   },
@@ -30,7 +34,7 @@ const projects = [
     i: 3,
     image: cocktailsapi,
     title: "COCKTAILs API",
-    description: "#React",
+    description: [{ j: "1", technology: "React Js" }],
     liveLink: "https://cocktails-api-pjrepo.vercel.app/",
     sourceCode: "https://github.com/pjrepo/cocktails",
   },
@@ -38,7 +42,7 @@ const projects = [
     i: 4,
     image: admindashboard,
     title: "Admin Dashboard",
-    description: "#React",
+    description: [{ j: "1", technology: "React Js" }],
     liveLink: "https://admin-dashboard-pjrepo.vercel.app/",
     sourceCode: "https://github.com/pjrepo/admin-dashboard",
   },
@@ -46,7 +50,10 @@ const projects = [
     i: 5,
     image: snakegame,
     title: "RETRO SNAKE GAME",
-    description: "#Typescript #React",
+    description: [
+      { j: "1", technology: "TypeScript" },
+      { j: "2", technology: "React Js" },
+    ],
     liveLink: "https://retro-snake-game-pjrepo.vercel.app/",
     sourceCode: "https://github.com/pjrepo/retro-snake-game",
   },
@@ -54,7 +61,10 @@ const projects = [
     i: 6,
     image: sortingvisualizer,
     title: "SORTING VISUALIZER",
-    description: "#Algorithms #React",
+    description: [
+      { j: "1", technology: "Algorithms" },
+      { j: "2", technology: "React Js" },
+    ],
     liveLink: "https://dribbble.com/Alien_pixels",
     sourceCode: "https://github.com",
   },
@@ -62,23 +72,42 @@ const projects = [
     i: 7,
     image: cryptotracker,
     title: "Cryptocurrency Tracker",
-    description: "#React #Material UI",
+    description: [
+      { j: "1", technology: "React Js" },
+      { j: "2", technology: "Material UI" },
+    ],
     liveLink: "https://cryptix-pjrepo.vercel.app/",
     sourceCode: "https://github.com/pjrepo/cryptix",
   },
   {
     i: 8,
+    image: portfoliowebsite,
+    title: "PORTFOLIO WEBSITE",
+    description: [{ j: "1", technology: "React Js" }],
+    liveLink: "https://cryptix-pjrepo.vercel.app/",
+    sourceCode: "https://github.com/pjrepo/portfolio",
+  },
+  {
+    i: 9,
     image: taskmanager,
     title: "TASK MANAGER",
-    description: "#Node #React #Mongo DB",
+    description: [
+      { j: "1", technology: "Node Js" },
+      { j: "2", technology: "React Js" },
+      { j: "3", technology: "Mongo DB" },
+    ],
     liveLink: "https://dribbble.com/Alien_pixels",
     sourceCode: "https://github.com",
   },
   {
-    i: 9,
+    i: 10,
     image: expensetracker,
     title: "EXPENSE TRACKER",
-    description: "#Node #React #Mongo DB",
+    description: [
+      { j: "1", technology: "Node Js" },
+      { j: "2", technology: "React Js" },
+      { j: "3", technology: "Mongo DB" },
+    ],
     liveLink: "https://dribbble.com/Alien_pixels",
     sourceCode: "https://github.com",
   },
@@ -99,16 +128,26 @@ const Portfolio = () => {
                 <div className="portfolio__item-image">
                   <img src={image} alt="" />
                   <h3>{title}</h3>
-                  <p>{description}</p>
+                  <p>
+                    {description.map((skill) => {
+                      const { j, technology } = skill;
+                      return (
+                        <span key={j}>
+                          #<b>{technology}</b>&nbsp; &nbsp;
+                        </span>
+                      );
+                    })}
+                  </p>
                   <div className="portfolio__item-cta">
-                    {title !== "COLOR PICKER EXTENSION" && (
-                      <div
-                        className="btn btn-primary"
-                        onClick={() => window.open(liveLink, "_blank")}
-                      >
-                        view
-                      </div>
-                    )}
+                    {title !== "COLOR PICKER EXTENSION" &&
+                      title !== "PORTFOLIO WEBSITE" && (
+                        <div
+                          className="btn btn-primary"
+                          onClick={() => window.open(liveLink, "_blank")}
+                        >
+                          view
+                        </div>
+                      )}
                     <div
                       className="btn"
                       onClick={() => window.open(sourceCode, "_blank")}
