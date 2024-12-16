bdf9c41951486860500ce42d0d9f759de24805d8
pjrepo <104340518+pjrepo@users.noreply.github.com>
2024-12-16 12:57:28 +0530

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,6 +8,11 @@ const About = () => {
       <h5>Let's know</h5>
       <h2>About Me</h2>
       <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={MYIMAGE} alt="about" />
+          </div>
+        </div>
         <div className="about__content">
           <p>
               I am a skilled Software Engineer with a strong foundation in web development, specializing in building high-performance and scalable applications. With over one and half years of experience, I have contributed to a variety of projects, including headless e-commerce solutions and innovative tools for client onboarding and background verification.
