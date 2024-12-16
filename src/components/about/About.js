31bcc31f860b83a97ad3b054200b0b319e19f094
pjrepo <104340518+pjrepo@users.noreply.github.com>
2024-12-16 13:01:13 +0530

    Final About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,10 +15,10 @@ const About = () => {
         </div>
         <div className="about__content">
           <p>
-              I am a skilled Software Engineer with a strong foundation in web development, specializing in building high-performance and scalable applications. With over one and half years of experience, I have contributed to a variety of projects, including headless e-commerce solutions and innovative tools for client onboarding and background verification.
+              I am a skilled Software Engineer with a <b style={{ color: "white" }}>strong foundation in web development</b>, specializing in building high-performance and scalable applications. With over one and half years of experience, I have contributed to a variety of projects, including headless e-commerce solutions and innovative tools for client onboarding and background verification.
           </p>
           <p>
-              I hold a B.Tech in Electrical and Electronics Engineering from NIT Warangal, where I developed a solid understanding of problem-solving, algorithms, and technology. My professional journey has been enriched by working with cutting-edge technologies like React.js, Django, GraphQL, and BigCommerce, delivering impactful solutions for clients and businesses. As a BigCommerce BigDev-certified professional, I have a proven track record of leveraging the platform to create tailored e-commerce solutions that meet diverse business needs
+              I hold a <b style={{ color: "white" }}B.Tech in Electrical and Electronics Engineering from NIT Warangal</b>, where I developed a solid understanding of problem-solving, algorithms, and technology. My professional journey has been enriched by working with <b style={{ color: "white" }}cutting-edge technologies like React.js, Django, GraphQL, and BigCommerce</b>, delivering impactful solutions for clients and businesses. As a <b style={{ color: "white" }}BigCommerce BigDev-certified professional</b>, I have a proven track record of leveraging the platform to create tailored e-commerce solutions that meet diverse business needs
           </p>
           <p>
               Notable achievements include qualifying for the UPSC Civil Services (Main) Examination, and earning top rankings in competitive exams like JEE Mains and AP EAMCET.
