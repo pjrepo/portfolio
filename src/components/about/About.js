e79260f4fe568c6ff4bbb666159d89c37c02837c
pjrepo <104340518+pjrepo@users.noreply.github.com>
2024-12-16 15:11:02 +0530

    checking

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -17,15 +17,6 @@ const About = () => {
           <p>
               I am a skilled Software Engineer with a <b style={{ color: "white" }}>strong foundation in web development</b>, specializing in building high-performance and scalable applications. With over one and half years of experience, I have contributed to a variety of projects, including headless e-commerce solutions and innovative tools for client onboarding and background verification.
           </p>
-          <p>
-              I hold a <b style={{ color: "white" }}B.Tech in Electrical and Electronics Engineering from NIT Warangal</b>, where I developed a solid understanding of problem-solving, algorithms, and technology. My professional journey has been enriched by working with <b style={{ color: "white" }}cutting-edge technologies like React.js, Django, GraphQL, and BigCommerce</b>, delivering impactful solutions for clients and businesses. As a <b style={{ color: "white" }}BigCommerce BigDev-certified professional</b>, I have a proven track record of leveraging the platform to create tailored e-commerce solutions that meet diverse business needs
-          </p>
-          <p>
-              Notable achievements include qualifying for the UPSC Civil Services (Main) Examination, and earning top rankings in competitive exams like JEE Mains and AP EAMCET.
-          </p>
-          <p>
-              I am passionate about solving complex problems, mentoring peers, and continuously expanding my knowledge in the tech space. When I'm not coding, I enjoy exploring innovative ideas and staying up-to-date with the latest trends in software development.
-          </p>
           <a href="#contact" className="btn btn-primary">
             Let' Talk
           </a>
