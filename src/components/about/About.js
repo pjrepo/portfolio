1ab52ae5d14ba1d95ccc11bacf7c3e2a9c2abc81
pjrepo <104340518+pjrepo@users.noreply.github.com>
2024-12-16 12:40:47 +0530

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,16 +15,16 @@ const About = () => {
         </div>
         <div className="about__content">
           <p>
-            Hi, I'm Pavan, an Electrical Engineering graduate from{" "}
-            <b style={{ color: "white" }}>NIT Warangal</b> (2016-2020) living in
-            Visakhapatnam. I'm a{" "}
-            <b style={{ color: "white" }}>Full Stack developer</b> with a focus
-            on the <b style={{ color: "white" }}>MERN stack</b>, but still
-            exploring other technologies and the frameworks that catch my
-            interest!
+              I am a skilled Software Engineer with a strong foundation in web development, specializing in building high-performance and scalable applications. With over one and half years of experience, I have contributed to a variety of projects, including headless e-commerce solutions and innovative tools for client onboarding and background verification.
           </p>
           <p>
-              I am a skilled Software Engineer with a strong foundation in web development, specializing in building high-performance and scalable applications. With over one and half years of experience, I have contributed to a variety of projects, including headless e-commerce solutions and innovative tools for client onboarding and background verification.
+              I hold a B.Tech in Electrical and Electronics Engineering from NIT Warangal, where I developed a solid understanding of problem-solving, algorithms, and technology. My professional journey has been enriched by working with cutting-edge technologies like React.js, Django, GraphQL, and BigCommerce, delivering impactful solutions for clients and businesses. As a BigCommerce BigDev-certified professional, I have a proven track record of leveraging the platform to create tailored e-commerce solutions that meet diverse business needs
+          </p>
+          <p>
+              Notable achievements include qualifying for the UPSC Civil Services (Main) Examination, and earning top rankings in competitive exams like JEE Mains and AP EAMCET.
+          </p>
+          <p>
+              I am passionate about solving complex problems, mentoring peers, and continuously expanding my knowledge in the tech space. When I'm not coding, I enjoy exploring innovative ideas and staying up-to-date with the latest trends in software development.
           </p>
           <p>
             From being a Branch Representative in university to a web developer,
